6b6cf49a9e829e08f642263e5a4062f1856e8f35
Alisson Araújo Mariano <157244142+Alissonaraujodev@users.noreply.github.com>
2024-11-26 22:28:09 -0300

    deploy do projeto

--- server.js
+++ server.js
@@ -1,16 +1,14 @@
 import express from "express";
 import routes from "./src/routes/postsRoutes.js";
 
-// Cria uma instância do Express, que será o núcleo da nossa aplicação
 const app = express();
+app.use(express.static("uploads"))
 routes(app)
 
-// Inicia o servidor na porta 3000 e mostra uma mensagem no console
-app.listen(3000, () =>{
+// Inicia o servidor na porta 3000 e exibe uma mensagem no console
+app.listen(3000, () => {
     console.log("Servidor escutando...");
 });
-
-
 
 
 
